0f14decb85ff48f2bbe497e22b23b5d772301571
joacoder <joacoder.dev@gmail.com>
2022-11-06 07:29:20 -0300

    ProyectoFinal3_0

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_API_KEY,
-  authDomain: REACT_APP_AUTH_DOMAIN,
-  projectId: REACT_APP_PROJECT_ID,
-  storageBucket:  REACT_APP_STORAGE,
-  messagingSenderId: REACT_APP_MESSAGING,
-  appId: REACT_APP_APP_ID,
+  apiKey: "AIzaSyAkMdHfXNQ3RyK1rMPF6_3t4oOMBiB0aGA",
+  authDomain: "tiendajoacoder.firebaseapp.com",
+  projectId: "tiendajoacoder",
+  storageBucket: "tiendajoacoder.appspot.com",
+  messagingSenderId: "564169473502",
+  appId: "1:564169473502:web:ae050d8c1c2a911b4bf14f"
 };
 
 const app = initializeApp(firebaseConfig);
